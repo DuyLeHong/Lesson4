474d7d257655d89e354222374e900085a710a825
duyle <leduy.av22@gmail.com>
2021-09-02 21:00:09 +0700

    update RecycleView

--- app/src/main/java/com/example/lesson4/MainActivity.java
+++ app/src/main/java/com/example/lesson4/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lesson4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.studentsList);
 
-        students = new ArrayList<Student>();
+        students = new ArrayList<>();
         //Tự phát sinh 50 dữ liệu mẫu
         for (int i = 1; i <= 50; i++) {
             students.add(new Student("Student Name"+i , 1995 + (i % 2)));
@@ -31,7 +33,14 @@ public class MainActivity extends AppCompatActivity {
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
 
+        //linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+
+        //GridLayoutManager gridLayoutManager = new GridLayoutManager(this);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        gridLayoutManager.setOrientation(GridLayoutManager.VERTICAL);
+
         recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setLayoutManager(gridLayoutManager);
     }
 }
